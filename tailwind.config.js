6f2d4ee03d06bb53c8e3a8680b6b4a49c33f3ee2
Ryiandika <vellixavalon.com>
2023-10-12 13:13:28 +0700

    .

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./landingpage.html, ./Pesanjasa.html, ./Loginsignin.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
